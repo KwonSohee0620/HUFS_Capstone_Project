d5dfe3603a7a6676e6f81377183d8e18e8fc62d3
ywon <joy323129@gmail.com>
2022-04-16 09:34:51 +0900

    add

--- app/src/main/java/com/example/trend/Login.kt
+++ app/src/main/java/com/example/trend/Login.kt
@@ -5,13 +5,11 @@ import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
-import android.provider.Settings
 import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
@@ -27,6 +25,7 @@ class Login : AppCompatActivity() {
 
         val id : EditText = findViewById<EditText>(R.id.edit_id)
         val pw : EditText = findViewById<EditText>(R.id.edit_pw)
+        val url: String = "http://10.0.2.2/loginn.php";
 
         btn_register.setOnClickListener(){
             val intent = Intent(this,Login2::class.java)
@@ -36,20 +35,7 @@ class Login : AppCompatActivity() {
         btn_login.setOnClickListener(){
             var userID: String = id.text.toString()
             var userPW: String = pw.text.toString()
-            var requestQueue: RequestQueue?=null
-
-            val url: String = "http://localhost/login.php";
-            requestQueue= Volley.newRequestQueue(applicationContext)
-
             loginVolley(this, url, userID, userPW)
-
-            if(userID=="yewon" && userPW=="0522"){
-                dialog("success")
-            }
-            else{
-                dialog("fail")
-            }
-
         }
     }
     private fun loginVolley(context: Context, url: String, userid: String, password: String) {
@@ -57,10 +43,10 @@ class Login : AppCompatActivity() {
         // 1. RequestQueue 생성 및 초기화
         val requestQueue = Volley.newRequestQueue(context)
 
-        // 2. Request Obejct인 StringRequest 생성
+        // 2. Request Object인 StringRequest 생성
         val request: StringRequest = object : StringRequest(Method.POST, url,
             Response.Listener { response ->
-                showJSONList(response)
+                showJSONList(response, userid, password)
             },
             Response.ErrorListener { error ->
                 Toast.makeText(context, error.toString(), Toast.LENGTH_LONG).show()
@@ -70,36 +56,31 @@ class Login : AppCompatActivity() {
                 val params: MutableMap<String,String> = HashMap()
                 params["userid"] = userid
                 params["password"] = password
-                params["uID"] = Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
                 return params
             }
         }
         requestQueue.add(request)
     }
 
-    fun showJSONList(response: String) {
+    private fun showJSONList(response: String, userid: String, password: String) {
         try {
             Log.e("TAG",response)
             val jsonObject = JSONObject(response)
-            jsonObject.let {
-                if(it.getString("userinfo") == null) {
-                    //showAlert(it.getString("result").toString(), it.getString("message").toString())
-                } else {
-                    if(it.getString("result").toString().equals("success")){
-                        val jsonInfo = JSONObject(it.getString("userinfo").toString())
-                        jsonInfo.let{
-                            // Preference 에 대한 정보 기록은 생략한다.
-                            startActivity(Intent(this,MainActivity::class.java))
-                        }
-                    }
-                }
+            if(jsonObject.getString("id").toString() == userid && jsonObject.getString("pw").toString() == password) {
+                dialog("success")
+                startActivity(Intent(this, MainActivity::class.java))
             }
+
+            else {
+                dialog("fail")
+            }
+
         } catch (e: JSONException) {
             e.printStackTrace()
         }
     }
 
-    fun dialog(type: String){
+    private fun dialog(type: String){
         var dialog = AlertDialog.Builder(this)
 
         if(type.equals("success")){
